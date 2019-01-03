fbe462bb0f43859e2d346fe0c8b8b028ba048fc3
Lubos Kejzlar <kejzlar@civ.zcu.cz>
2019-01-03 07:43:47 +0100

    Updated: run_test.py (#2)

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,4 +1,15 @@
+from __future__ import print_function
 import os
 
+path = '..'
 dirpath = os.getcwd()
-print("current directory is : " + dirpath)
+
+print("TEST: BEGIN\n")
+print("TEST: current directory is : " + dirpath)
+print("TEST: contents of .. is : ")
+
+files = os.listdir(path)
+for name in files:
+    print(name)
+
+print("TEST: END\n")
